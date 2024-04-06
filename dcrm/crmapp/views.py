939167fac0fe0f2c3873dd7892e2406ddcbb02b5
Yohannes <jhon.klaus2008@gmail.com>
2024-04-06 15:23:31 +0300

    show individual records

--- dcrm/crmapp/views.py
+++ dcrm/crmapp/views.py
@@ -42,3 +42,12 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.error(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
